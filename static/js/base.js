39c738069d502a3d18699dd5385a544356165159
WaksKevin <wakskevin@outlook.com>
2023-12-17 19:53:22 +0300

    removed .gitignore | tracking static folder

---
+++ static/js/base.js
@@ -0,0 +1,80 @@
+document.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+    /**
+     * Preloader
+     */
+    const preloader = document.querySelector('#preloader');
+    if (preloader) {
+        window.addEventListener('load', () => {
+            preloader.remove();
+        });
+    }
+
+    /**
+     * Scroll top button
+     */
+    let scrollTop = document.querySelector('.scroll-top');
+
+    function toggleScrollTop() {
+        if (scrollTop) {
+            window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+        }
+    }
+    scrollTop.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+
+    window.addEventListener('load', toggleScrollTop);
+    document.addEventListener('scroll', toggleScrollTop);
+
+    /**
+     * Top Bar and Header on Scroll
+     */
+    const header = document.querySelector("#header");
+
+    function toggleHeaderOnScroll() {
+        if (header) {
+            window.scrollY > 40 ? header.classList.add("header-onscroll") : header.classList.remove("header-onscroll");
+        }
+    }
+
+    document.addEventListener('scroll', toggleHeaderOnScroll);
+
+    /**
+     * Mobile nav toggle
+     */
+    const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+
+    function mobileNavToogle() {
+        document.querySelector('body').classList.toggle('mobile-nav-active');
+        mobileNavToggleBtn.classList.toggle('bi-list');
+        mobileNavToggleBtn.classList.toggle('bi-x');
+    }
+    mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+
+    /**
+     * Footer Copyright
+     */
+    var today = new Date();
+    var year = today.getFullYear();
+    const copyright = document.querySelector("#footer small");
+    copyright.textContent += ` ${year}`;
+
+    /**
+     * Animation on scroll function and init
+     */
+    function aosInit() {
+        AOS.init({
+            duration: 700,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        });
+    }
+    window.addEventListener('load', aosInit);
+
+});
